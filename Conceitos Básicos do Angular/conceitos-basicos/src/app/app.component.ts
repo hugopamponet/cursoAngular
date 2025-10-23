ecb769aa217656332e2750f19dcec76443da223f
hugopamponet <hugofarias9@gmail.com>
2025-10-23 15:45:16 -0300

    first commit

---
+++ Conceitos Básicos do Angular/conceitos-basicos/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HelloWorldComponent } from './helloword/helloworld.component';
+import { MinhaPaginaComponent } from './minha-pagina/minha-pagina.component'
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, HelloWorldComponent, MinhaPaginaComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'conceitos-basicos';
+}
