241523f26e2dd690f71147ccb2920b26b90fdad8
hugopamponet <hugofarias9@gmail.com>
2025-10-25 12:28:02 -0300

    feat: add input field actions for calculator

--- Conceitos Básicos do Angular/calculadora/src/app/calculator/calculator.component.ts
+++ Conceitos Básicos do Angular/calculadora/src/app/calculator/calculator.component.ts
@@ -8,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class CalculatorComponent {
 
+  numero1: number = 0;
+  numero2: number = 0;
+  result: number = 0;
+
+  calcularResultado() {
+    this.result = this.numero1 + this.numero2;
+  }
 }
