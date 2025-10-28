ca646b3f487fceed050f6f61917ec46ab2a55b18
hugopamponet <hugofarias9@gmail.com>
2025-10-28 16:12:38 -0300

    feat: add functionality to strike through item when checkbox is selected

--- Conceitos Básicos do Angular/conceitos-basicos/src/app/lista-compras/lista-compras.component.ts
+++ Conceitos Básicos do Angular/conceitos-basicos/src/app/lista-compras/lista-compras.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ItemLista } from './itemlista'
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-lista-compras',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './lista-compras.component.html',
   styleUrl: './lista-compras.component.scss'
 })
@@ -20,5 +21,9 @@ export class ListaComprasComponent {
 
     this.lista.push(itemLista);
     this.item = '';
+  }
+
+  riscarItem(itemLista: ItemLista) {
+    itemLista.comprado = !itemLista.comprado;
   }
 }
